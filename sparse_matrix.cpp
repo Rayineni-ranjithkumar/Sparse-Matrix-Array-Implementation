cbfd6b9664ff753426016c250f04ecac54d4de38
rayineni ranjith Kumar <ranjithnani1299@gmail.com>
2025-09-10 19:26:12 -0500

    Update sparse_matrix.cpp

--- sparse_matrix.cpp
+++ sparse_matrix.cpp
@@ -1,59 +1,43 @@
 #include <iostream>
 using namespace std;
-
-// Structure to store non-zero elements
-struct Element {
-    int row;    // Row index
-    int col;    // Column index
-    int value;  // Non-zero value
+struct Element { // we can store the non-zero elements in elements
+    int row;  
+    int col;   
+    int value; 
 };
-
 class SparseMatrix {
 private:
     int rows, cols, nonZeroCount;
-    Element *elements; // Dynamic array to store non-zero elements
-
+    Element *elements;
 public:
-    // Constructor to initialize matrix details
     SparseMatrix(int r, int c, int count) {
         rows = r;
         cols = c;
         nonZeroCount = count;
-        elements = new Element[nonZeroCount]; // Allocate memory
+        elements = new Element[nonZeroCount]; // here we can allocate memory
     }
-
-    // Destructor to release allocated memory
-    ~SparseMatrix() {
+    ~SparseMatrix() { // this is for destructor to release allocated memory
         delete[] elements;
     }
-
-    // Function to read non-zero elements from user
+    //here we are writeing Function to read non-zero elements from user like user giving inputs that function read.
     void readElements() {
-        cout << "\nEnter the details of non-zero elements:\n";
+        cout << "\nranjith plz Enter the details of non-zero elements:\n";
         cout << "(Row, Column, Value)\n";
         for (int i = 0; i < nonZeroCount; i++) {
             cout << "Element " << i + 1 << ": ";
             cin >> elements[i].row >> elements[i].col >> elements[i].value;
         }
     }
-
-    // Function to display the sparse matrix representation
-    void displaySparse() {
-        cout << "\nSparse Matrix Representation:\n";
+    void displaySparse() { // this is for our Function to display the sparse matrix
+        cout << "\nranjith your Sparse Matrix is :\n";
         cout << "Row\tCol\tValue\n";
-        cout << "-------------------------\n";
         for (int i = 0; i < nonZeroCount; i++) {
             cout << elements[i].row << "\t" << elements[i].col << "\t" << elements[i].value << "\n";
         }
     }
-
-    // Function to display the complete matrix
-    void displayFullMatrix() {
-        cout << "\nFull Matrix Representation:\n";
-        cout << "---------------------------\n";
-        int k = 0; // Index to track non-zero elements
-
-        // Traverse through each position in the matrix
+    void displayFullMatrix() {  // this is for our Function to display the complete matrix
+        cout << "\n ranjith your Full Matrix is :\n";
+        int k = 0;
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
                 if (k < nonZeroCount && elements[k].row == i && elements[k].col == j) {
@@ -67,26 +51,18 @@ public:
         }
     }
 };
-
 int main() {
     int rows, cols, nonZeroCount;
-
-    cout << "Enter number of rows: ";
+    cout << "ranjith enter number of rows buddy: ";
     cin >> rows;
-
-    cout << "Enter number of columns: ";
+    cout << "ranjith Enter number of columns buddy: ";
     cin >> cols;
-
-    cout << "Enter number of non-zero elements: ";
+    cout << "ranjith Enter number of non-zero elements buddy: ";
     cin >> nonZeroCount;
-
-    // Create sparse matrix object
-    SparseMatrix sm(rows, cols, nonZeroCount);
-
-    // Read, store, and display results
-    sm.readElements();
-    sm.displaySparse();
-    sm.displayFullMatrix();
-
+    // here object we are creating for sparse matrix..
+    SparseMatrix ourresult(rows, cols, nonZeroCount);
+    ourresult.readElements();
+    ourresult.displaySparse();
+    ourresult.displayFullMatrix();
     return 0;
 }
